a488ad96f12f5618ceb49bd2f0a5ef7bf7bd0019
Andrei <gagarin1982@gmail.com>
2022-03-13 11:43:06 +0500

    Ready

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+const val MINUTE = 60
+const val HOUR = 60 * 60
+const val DAY = 24 * 60 * 60
+const val TWO_DAYS = DAY * 2
+const val TREE_DAYS = DAY * 3
+
+fun main() {
+    val timeInSeconds = readln().toInt()
+
+    println(agoToText(timeInSeconds))
+}
+
+fun agoToText(seconds: Int): String {
+    return when (seconds) {
+        in 0..MINUTE -> "Just now"
+        in MINUTE + 1..HOUR -> "${seconds / MINUTE} ${minute(seconds)} назад"
+        in HOUR + 1..DAY -> "${seconds / HOUR} ${hour(seconds)} назад"
+        in DAY + 1..TWO_DAYS -> "Today"
+        in TWO_DAYS + 1..TREE_DAYS -> "Yesterday"
+        else -> "For a long time"
+    }
+}
+
+fun minute(seconds: Int): String {
+    val minutes = seconds / MINUTE
+    return when {
+        minutes % 10 == 1 -> "минуту"
+        minutes % 10 == 2 -> "минуты"
+        else -> "минут"
+    }
+}
+
+fun hour(seconds: Int): String {
+    val hours = seconds / HOUR
+    return when {
+        hours % 10 == 1 -> "час"
+        hours % 10 == 2 -> "часа"
+        else -> "часов"
+    }
+}
